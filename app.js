9d17902082aadf3020c3c5f3259410815dc0326b
Jack Joseph <dunleavyjack@gmail.com>
2020-11-09 10:45:15 +0900

    Updated delete method

--- app.js
+++ app.js
@@ -54,11 +54,17 @@ app.get('/campgrounds/:id/edit', async (req, res) => {
 });
  
 app.put('/campgrounds/:id', async (req, res) =>{
-    const {id} = req.params;
+    const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
     res.redirect(`/campgrounds/${campground._id}`)
 })
- 
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds')
+})
+
 app.listen(3000, ()=>  {
     console.log('Server running on port 3000')
 });
