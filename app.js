7da4a5dbac6408bfe1e101b57e917320265dd20a
Jack Joseph <dunleavyjack@gmail.com>
2020-11-11 22:49:53 +0900

    Added joi

--- app.js
+++ app.js
@@ -76,8 +76,8 @@ app.all('*', (req, res, next) => {
 
 app.use((err, req, res, next) => {
     const { statusCode = 500, message = 'Something went wrong' } = err;
-    res.status(statusCode).send(message)
-    res.send('Oh boy, something went wrong!')
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
 })
 
 app.listen(3000, ()=>  {
