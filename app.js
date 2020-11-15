5ea95bcd64b15f30ac3a8de1f6de03fa5f72815e
Jack Joseph <dunleavyjack@gmail.com>
2020-11-15 23:01:25 +0900

    Added user model

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const session = require('express-session');
 const flash = require('connect-flash')
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local')
+const User = require('./models/user')
 
 
 const campgrounds = require('./routes/campgrounds')
@@ -34,6 +37,8 @@ app.use(express.urlencoded({ extended: true}))
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
 
+
+
 const sessionConfig = {
     secret: 'thisshouldbeabettersecret',
     resave: false,
@@ -44,13 +49,27 @@ const sessionConfig = {
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
-app.use(session(sessionConfig))
-app.use(flash())
+
+app.use(session(sessionConfig));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser())
 
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
+})
+
+app.get('/fakeUser', async (req, res) => {
+    const user = new User({ email: 'lll@gmail.com', username: 'lll' })
+    const newUser = await User.register(user, 'pass')
+    res.send(newUser)
 })
 
 app.use('/campgrounds', campgrounds)
