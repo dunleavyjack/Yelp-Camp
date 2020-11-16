24e769fe33dfc239a9fe3af4a437f8a10d0afca1
Jack Joseph <dunleavyjack@gmail.com>
2020-11-16 14:28:03 +0900

    Organized middleware

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,28 +1,10 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
-
-//models
+const { validateReview } = require('../middleware');
 const Campground = require('../models/campground');
 const Review = require('../models/review')
-
-//schemas
-const { reviewSchema } = require('../schemas.js');
-
-
-//utils 
 const ExpressError = require('../utils/ExpressError');
 const catchAsync = require('../utils/catchAsync');
-
-
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body)
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
 
 router.post('/', validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
